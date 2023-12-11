f1fb9e9fcd572c1de87d5c1265f3fb6329443820
Emran321 <rony.ehossain@gmail.com>
2023-12-11 15:11:16 +0600

    Leave Type Model added and Mg added

---
+++ app.Infrastructure/Migrations/20231211090625_DropdownModelNameUpdated.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace app.Infrastructure.Migrations
+{
+    public partial class DropdownModelNameUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LeaveBalance",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LeaveQty = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    LeaveCategoryId = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LeaveBalance", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LeaveBalance_LeaveCategory_LeaveCategoryId",
+                        column: x => x.LeaveCategoryId,
+                        principalSchema: "dbo",
+                        principalTable: "LeaveCategory",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2c5e174e-3b0e-446f-86af-483d56fd7210",
+                column: "ConcurrencyStamp",
+                value: "61fec71c-b2b3-4cba-a840-f28f860ea89d");
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "8e445865-a24d-4543-a6c6-9443d048cdb9",
+                column: "ConcurrencyStamp",
+                value: "350301e1-f7ed-4d89-90d6-db9bfad85ec7");
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "MainMenu",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 15, 6, 24, 834, DateTimeKind.Local).AddTicks(4473));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "MainMenu",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 15, 6, 24, 834, DateTimeKind.Local).AddTicks(4484));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 15, 6, 24, 834, DateTimeKind.Local).AddTicks(4499));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 15, 6, 24, 834, DateTimeKind.Local).AddTicks(4500));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 15, 6, 24, 834, DateTimeKind.Local).AddTicks(4502));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 15, 6, 24, 834, DateTimeKind.Local).AddTicks(4503));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 5L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 15, 6, 24, 834, DateTimeKind.Local).AddTicks(4504));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 6L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 15, 6, 24, 834, DateTimeKind.Local).AddTicks(4505));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 7L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 15, 6, 24, 834, DateTimeKind.Local).AddTicks(4506));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "UserPermissions",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 15, 6, 24, 834, DateTimeKind.Local).AddTicks(4520));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "UserPermissions",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 15, 6, 24, 834, DateTimeKind.Local).AddTicks(4522));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "UserPermissions",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 15, 6, 24, 834, DateTimeKind.Local).AddTicks(4524));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "UserPermissions",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 15, 6, 24, 834, DateTimeKind.Local).AddTicks(4525));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "UserPermissions",
+                keyColumn: "Id",
+                keyValue: 5L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 15, 6, 24, 834, DateTimeKind.Local).AddTicks(4526));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "UserPermissions",
+                keyColumn: "Id",
+                keyValue: 6L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 15, 6, 24, 834, DateTimeKind.Local).AddTicks(4527));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "UserPermissions",
+                keyColumn: "Id",
+                keyValue: 7L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 15, 6, 24, 834, DateTimeKind.Local).AddTicks(4527));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveBalance_LeaveCategoryId",
+                schema: "dbo",
+                table: "LeaveBalance",
+                column: "LeaveCategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LeaveBalance",
+                schema: "dbo");
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2c5e174e-3b0e-446f-86af-483d56fd7210",
+                column: "ConcurrencyStamp",
+                value: "8b0d1a44-d7a8-45b8-b499-8c6aa3e188b5");
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "8e445865-a24d-4543-a6c6-9443d048cdb9",
+                column: "ConcurrencyStamp",
+                value: "31f98bbf-440d-4cbd-a85b-6f74673afe24");
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "MainMenu",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 12, 18, 29, 438, DateTimeKind.Local).AddTicks(7156));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "MainMenu",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 12, 18, 29, 438, DateTimeKind.Local).AddTicks(7167));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 12, 18, 29, 438, DateTimeKind.Local).AddTicks(7185));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 12, 18, 29, 438, DateTimeKind.Local).AddTicks(7187));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 12, 18, 29, 438, DateTimeKind.Local).AddTicks(7188));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 12, 18, 29, 438, DateTimeKind.Local).AddTicks(7189));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 5L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 12, 18, 29, 438, DateTimeKind.Local).AddTicks(7191));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 6L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 12, 18, 29, 438, DateTimeKind.Local).AddTicks(7192));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "MenuItem",
+                keyColumn: "Id",
+                keyValue: 7L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 12, 18, 29, 438, DateTimeKind.Local).AddTicks(7193));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "UserPermissions",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 12, 18, 29, 438, DateTimeKind.Local).AddTicks(7211));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "UserPermissions",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 12, 18, 29, 438, DateTimeKind.Local).AddTicks(7212));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "UserPermissions",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 12, 18, 29, 438, DateTimeKind.Local).AddTicks(7213));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "UserPermissions",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 12, 18, 29, 438, DateTimeKind.Local).AddTicks(7214));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "UserPermissions",
+                keyColumn: "Id",
+                keyValue: 5L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 12, 18, 29, 438, DateTimeKind.Local).AddTicks(7215));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "UserPermissions",
+                keyColumn: "Id",
+                keyValue: 6L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 12, 18, 29, 438, DateTimeKind.Local).AddTicks(7216));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "UserPermissions",
+                keyColumn: "Id",
+                keyValue: 7L,
+                column: "CreatedOn",
+                value: new DateTime(2023, 12, 11, 12, 18, 29, 438, DateTimeKind.Local).AddTicks(7217));
+        }
+    }
+}
