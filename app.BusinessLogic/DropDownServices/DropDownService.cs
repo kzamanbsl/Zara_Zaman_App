7b3b8590839097205f65afa85b52c27a4a390d32
Emran321 <rony.ehossain@gmail.com>
2023-12-12 12:54:00 +0600

    employeeServiceAdd

--- app.BusinessLogic/DropDownServices/DropDownService.cs
+++ app.BusinessLogic/DropDownServices/DropDownService.cs
@@ -25,6 +25,7 @@ namespace app.Services.DropdownServices
             return dropDownViewModels;
         }
 
+
         public async Task<IEnumerable<DropdownViewModel>> DepartmentSelectionList()
         {
             IEnumerable<DropdownViewModel> dropDownViewModels = await Task.Run(() => (from t1 in _dbContext.Department
@@ -47,9 +48,60 @@ namespace app.Services.DropdownServices
             return dropDownViewModels;
         }
 
+     
+
         public async Task<IEnumerable<DropdownViewModel>> LeaveCategorySelectionList()
         {
             IEnumerable<DropdownViewModel> dropDownViewModels = await Task.Run(() => (from t1 in _dbContext.LeaveCategory
+                                                                                      select new DropdownViewModel
+                                                                                      {
+                                                                                          Id = t1.Id,
+                                                                                          Name = t1.Name
+                                                                                      }).AsQueryable());
+            return dropDownViewModels;
+        }
+
+
+        public async Task<IEnumerable<DropdownViewModel>> CountrySelectionList()
+        {
+            IEnumerable<DropdownViewModel> dropDownViewModels = await Task.Run(() => (from t1 in _dbContext.Country
+                                                                                      select new DropdownViewModel
+                                                                                      {
+                                                                                          Id = t1.Id,
+                                                                                          Name = t1.Name
+                                                                                      }).AsQueryable());
+            return dropDownViewModels;
+        }
+
+        public async Task<IEnumerable<DropdownViewModel>> DivisionSelectionList()
+        {
+            IEnumerable<DropdownViewModel> dropDownViewModels = await Task.Run(() => (from t1 in _dbContext.Division
+                                                                                      select new DropdownViewModel
+                                                                                      {
+                                                                                          Id = t1.Id,
+                                                                                          Name = t1.Name
+                                                                                      }).AsQueryable());
+            return dropDownViewModels;
+        }
+        public async Task<IEnumerable<DropdownViewModel>> DistrictSelectionList(int divisionId = 0)
+        {
+            IEnumerable<DropdownViewModel> dropDownViewModels = await Task.Run(() => (from t1 in _dbContext.District
+                                                                                      where divisionId > 0 ? t1.DivisionId == divisionId : t1.Id > 0
+                                                                                      select new DropdownViewModel
+                                                                                      {
+                                                                                          Id = t1.Id,
+                                                                                          Name = t1.Name
+                                                                                      }).AsQueryable()) ;
+            return dropDownViewModels;
+        }
+
+        public async Task<IEnumerable<DropdownViewModel>> UpazilaSelectionList(int divisionId = 0, int districtsId = 0)
+        {
+            IEnumerable<DropdownViewModel> dropDownViewModels = await Task.Run(() => (from t1 in _dbContext.Upazila
+                                                                                      join t2 in _dbContext.District on t1.DistrictId equals t2.Id
+                                                                                      where (divisionId > 0 && districtsId > 0 ) ? t2.DivisionId == divisionId && t1.DistrictId == districtsId 
+                                                                                      : (divisionId == 0 && districtsId > 0) ? t1.DistrictId == districtsId
+                                                                                      : t1.Id > 0
                                                                                       select new DropdownViewModel
                                                                                       {
                                                                                           Id = t1.Id,
--- app.BusinessLogic/DropDownServices/IDropDownService.cs
+++ app.BusinessLogic/DropDownServices/IDropDownService.cs
@@ -6,6 +6,10 @@
         Task<IEnumerable<DropdownViewModel>> DesignatinSelectionList();
         Task<IEnumerable<DropdownViewModel>> DepartmentSelectionList();
         Task<IEnumerable<DropdownViewModel>> LeaveCategorySelectionList();
+        Task<IEnumerable<DropdownViewModel>> CountrySelectionList();
+        Task<IEnumerable<DropdownViewModel>> DivisionSelectionList();
+        Task<IEnumerable<DropdownViewModel>> DistrictSelectionList(int divisionId =0);
+        Task<IEnumerable<DropdownViewModel>> UpazilaSelectionList(int divisionId = 0, int districtsId = 0);
 
     }
 }
