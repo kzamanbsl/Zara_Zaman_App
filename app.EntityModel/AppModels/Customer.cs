853a496fbb3ec2dc30b1ca02719054c0122c217d
Emran321 <rony.ehossain@gmail.com>
2023-12-28 18:15:57 +0600

    errorfixandProjectOrganized

---
+++ app.EntityModel/AppModels/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app.EntityModel.AppModels
+{
+    public class Customer : BaseEntity
+    {
+        public string Name { get; set; }    
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string? Email { get; set; }
+
+        public int? DivisionId { get; set; }
+        public Division Division { get; set; }
+        public int? DistrictId { get; set; }
+        public District District { get; set; }
+        public int? UpazilaId { get; set; }
+        public Upazila Upazila { get; set; }
+
+    }
+}
