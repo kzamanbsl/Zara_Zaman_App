d59615f404e52d5b560d90639f241fb23c88804c
Emran321 <rony.ehossain@gmail.com>
2023-12-31 11:41:00 +0600

    CustomerAndSupplierAdded

--- app.EntityModel/AppModels/Supplier.cs
+++ app.EntityModel/AppModels/Supplier.cs
@@ -8,5 +8,16 @@ namespace app.EntityModel.AppModels
 {
     public class Supplier : BaseEntity
     {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public int? DivisionId { get; set; }
+        public Division Division { get; set; }
+        public int? DistrictId { get; set; }
+        public District District { get; set; }
+        public int? UpazilaId { get; set; }
+        public Upazila Upazila { get; set; }
     }
 }
