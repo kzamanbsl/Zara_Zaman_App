b96e47fe25afb6116765c5656c9a191718948b80
Emran321 <rony.ehossain@gmail.com>
2023-12-31 15:57:49 +0600

    supplierModified

--- app.EntityModel/AppModels/Supplier.cs
+++ app.EntityModel/AppModels/Supplier.cs
@@ -13,12 +13,12 @@ namespace app.EntityModel.AppModels
         public string Email { get; set; }
         public string Description { get; set; }
         public string Address { get; set; }
+        public int? UpazilaId { get; set; }
+        public Upazila Upazila { get; set; }
         public int? DivisionId { get; set; }
         public Division Division { get; set; }
         public int? DistrictId { get; set; }
-        public District District { get; set; }
-        public int? UpazilaId { get; set; }
-        public Upazila Upazila { get; set; }
+        public District District { get; set; } 
         public Country Country { get; set; }
     }
 }
