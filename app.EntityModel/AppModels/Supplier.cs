8184062265d820b28baff3effc4e4f61dc5f335f
Emran321 <rony.ehossain@gmail.com>
2023-12-31 15:55:31 +0600

    supplierAdded

--- app.EntityModel/AppModels/Supplier.cs
+++ app.EntityModel/AppModels/Supplier.cs
@@ -19,5 +19,6 @@ namespace app.EntityModel.AppModels
         public District District { get; set; }
         public int? UpazilaId { get; set; }
         public Upazila Upazila { get; set; }
+        public Country Country { get; set; }
     }
 }
