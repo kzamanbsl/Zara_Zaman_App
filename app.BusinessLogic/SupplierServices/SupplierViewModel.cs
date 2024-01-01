9070163574c54dee31380747ad55758463f049a6
Md.Ataur Rahman <142331145+ataur481209@users.noreply.github.com>
2024-01-01 10:35:23 +0600

    Suplier model Sevice

--- app.BusinessLogic/SupplierServices/SupplierViewModel.cs
+++ app.BusinessLogic/SupplierServices/SupplierViewModel.cs
@@ -14,18 +14,18 @@ namespace app.Services.SupplierServices
         public string Description { get; set; }
         [DisplayName("Address")]
         public string Address { get; set; }
-        public long? UpazilaId { get; set; }
-        [DisplayName("Upazila Name")]
-        public string UpazilaName { get; set; }
+        public long? CountryId { get; set; }
+        [DisplayName("Country Name")]
+        public string CountryName { get; set; }
         public long? DivisionId { get; set; }
         [DisplayName("Division Name")]
         public string DivisionName { get; set; }
         public long? DistrictId { get; set; }
         [DisplayName("District Name")]
         public string DistrictName { get; set; }
-        public long? CountryId { get; set; }
-        [DisplayName("Country Name")]
-        public string CountryName { get; set; }
+        public long? UpazilaId { get; set; }
+        [DisplayName("Upazila Name")]
+        public string UpazilaName { get; set; }
 
         public IEnumerable<SupplierViewModel> SupplierList { get; set; }
     }
