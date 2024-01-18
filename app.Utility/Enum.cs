620d5aa10d58fd331f30b85fccfa1893c6cd1b02
Rakibul Islam <tuhinrakib3456@gmail.com>
2024-01-18 16:30:25 +0600

    AssetCategory added

--- app.Utility/Enum.cs
+++ app.Utility/Enum.cs
@@ -39,7 +39,7 @@
     }
 
 
-    public enum ProductcategoryTypeEnum
+    public enum ProductCategoryTypeEnum
     {
         ProductCategory = 1,
         AssetCategory
---
+++ app.WebApp/Controllers/AssetCategoryController.cs
@@ -0,0 +1,70 @@
+﻿using app.Services.DropdownServices;
+using app.Services.AssetCategoryServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace app.WebApp.Controllers
+{
+    public class AssetCategoryController : Controller
+    {
+
+        private readonly IAssetCategoryService _iService;
+        private readonly IDropdownService _dropdownService;
+        public AssetCategoryController(IAssetCategoryService iService, IDropdownService dropdownService)
+        {
+            _iService = iService;
+            _dropdownService = dropdownService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var result = await _iService.GetAllRecord();
+            return View(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddRecord()
+        {
+            AssetCategoryViewModel viewModel = new AssetCategoryViewModel();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRecord(AssetCategoryViewModel viewModel)
+        {
+            var result = await _iService.AddRecord(viewModel);
+            if (result == true)
+            {
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError(string.Empty, "Same Name already exists!");
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateRecord(long id)
+        {
+            var result = await _iService.GetRecordById(id);
+            return View(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateRecord(AssetCategoryViewModel model)
+        {
+            var result = await _iService.UpdateRecord(model);
+            if (result == true)
+            {
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError(string.Empty, "Same Name already exists!");
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(long id)
+        {
+            var res = await _iService.DeleteRecord(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
