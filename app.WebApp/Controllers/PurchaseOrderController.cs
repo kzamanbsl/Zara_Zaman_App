67f1dcbbf0a1929e9e9567927614f69ac922a806
Md.Ataur Rahman <142331145+ataur481209@users.noreply.github.com>
2024-02-04 09:29:20 +0600

    dropdown  selected

--- app.WebApp/Controllers/PurchaseOrderController.cs
+++ app.WebApp/Controllers/PurchaseOrderController.cs
@@ -102,6 +102,20 @@ namespace app.WebApp.Controllers
             return View(nameof(UpdatePurchaseOrder), viewModel);
         }
         [HttpGet]
+        public async Task<JsonResult> IsPurchaseOrderMasterUpdateRecord(int id)
+        {
+            var data = await _ipurchaseOrderService.GetRecordById(id);
+            return Json(data);
+
+            //bool IsExist = false;
+            //var data = await _ipurchaseOrderService.IsPurchaseOrderMasterUpdateRecord(id);
+            //if (data)
+            //{
+            //    IsExist = true;
+            //}
+            //return Json(IsExist);
+        }
+        [HttpGet]
         public async Task<IActionResult> PurchaseOrderMasterUpdateRecord(long id)
         {
 
