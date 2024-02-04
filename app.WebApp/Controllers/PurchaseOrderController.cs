c714eac6c071897fabe9d3796c02272e750e0df0
Md.Ataur Rahman <142331145+ataur481209@users.noreply.github.com>
2024-02-04 18:30:57 +0600

    dropdown  selected update

--- app.WebApp/Controllers/PurchaseOrderController.cs
+++ app.WebApp/Controllers/PurchaseOrderController.cs
@@ -102,7 +102,7 @@ namespace app.WebApp.Controllers
             return View(nameof(UpdatePurchaseOrder), viewModel);
         }
 
-        //
+        //[HttpGet]
         //public async Task<JsonResult> IsPurchaseOrderMasterUpdateRecord(int id)
         //{
         //    var data = await _ipurchaseOrderService.GetRecordById(id);
@@ -116,23 +116,24 @@ namespace app.WebApp.Controllers
         //    //}
         //    //return Json(IsExist);
         //}
-        [HttpPost]
-        public async Task<JsonResult> checkPurchaseOrderMasterUpdateRecord(PurchaseOrderViewModel model)
-        {
-            try
-            {
-                // Perform the necessary operations to update the purchase order record
-                // Example: _purchaseOrderService.UpdatePurchaseOrder(model);
 
-                // Return a success response
-                return Json(new { success = true });
-            }
-            catch (Exception ex)
-            {
-                // Return an error response
-                return Json(new { success = false, message = ex.Message });
-            }
-        }
+        //[HttpPost]
+        //public async Task<JsonResult> checkPurchaseOrderMasterUpdateRecord(PurchaseOrderViewModel model)
+        //{
+        //    try
+        //    {
+        //        // Perform the necessary operations to update the purchase order record
+        //        // Example: _purchaseOrderService.UpdatePurchaseOrder(model);
+
+        //        // Return a success response
+        //        return Json(new { success = true });
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // Return an error response
+        //        return Json(new { success = false, message = ex.Message });
+        //    }
+        //}
 
 
         [HttpGet]
