86df647f705681fc863828538f730243ed2a13dc
chittoranjan <chittoranjan.sarker@gmail.com>
2024-02-08 11:33:57 +0600

    add record updated

--- app.WebApp/Controllers/AssembleWorkController.cs
+++ app.WebApp/Controllers/AssembleWorkController.cs
@@ -39,6 +39,7 @@ namespace app.WebApp.Controllers
             ViewBag.EmployeeList = new MultiSelectList((await _iDropdownService.EmployeeSelectionList())
                 .Select(s => new { Id = s.Id, Name = s.Name }), "Id", "Name");
             AssembleWorkViewModel viewModel = new AssembleWorkViewModel();
+            viewModel.AssembleDate=DateTime.Now;
             return View(viewModel);
         }
 
