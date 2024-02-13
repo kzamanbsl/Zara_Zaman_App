578013f41047194333e781c3eec05666293e8fd6
Shimul-Aashraf <ashraf17cse@gmail.com>
2024-02-13 18:37:40 +0600

    Employee List Updated

--- app.BusinessLogic/EmployeeServices/IEmployeeService.cs
+++ app.BusinessLogic/EmployeeServices/IEmployeeService.cs
@@ -1,12 +1,18 @@
-﻿namespace app.Services.EmployeeServices
+﻿using app.EntityModel.DataTablePaginationModels;
+using app.Services.SupplierServices;
+
+namespace app.Services.EmployeeServices
 {
     public interface IEmployeeService
     {
         Task<bool> AddRecord(EmployeeViewModel vm);
         Task<bool> UpdateRecord(EmployeeViewModel vm);
         Task<EmployeeViewModel> GetRecordById(long id);
-        Task<EmployeeViewModel> GetAllRecord();
+      
         //Task<bool> DeleteRecord(long id);
         Task<EmployeeViewModel>GetDetailsById(long id);
+        Task<EmployeeViewModel> GetAllRecord();
+        Task<DataTablePagination<EmployeeSearchDto>> SearchAsync(DataTablePagination<EmployeeSearchDto> searchDto);
+
     }
 }
