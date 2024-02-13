d01d55d8e7cb0e909cb1e73563360e93f8224f0c
Shimul-Aashraf <ashraf17cse@gmail.com>
2024-02-13 11:07:53 +0600

    Company & ProductCategory formatting,data type changing

--- app.BusinessLogic/CompanyServices/ICompanyService.cs
+++ app.BusinessLogic/CompanyServices/ICompanyService.cs
@@ -6,7 +6,7 @@
         Task<bool> AddRecord(CompanyViewModel vm);
         Task<bool> UpdateRecord(CompanyViewModel vm);
         Task<CompanyViewModel> GetRecordById(long id);
-        Task<CompanyViewModel> GetAllRecord();
         Task<bool> DeleteRecord(long id);
+        Task<CompanyViewModel> GetAllRecord();
     }
 }
