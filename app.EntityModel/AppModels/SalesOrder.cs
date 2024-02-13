9d0400bb7c9bd29e12a26b6489380a396ec860e6
chittoranjan <chittoranjan.sarker@gmail.com>
2024-02-13 10:12:02 +0600

    sales and terms and condition model added

--- app.EntityModel/AppModels/SalesOrder.cs
+++ app.EntityModel/AppModels/SalesOrder.cs
@@ -1,23 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace app.EntityModel.AppModels
+﻿namespace app.EntityModel.AppModels
 {
     public class SalesOrder : BaseEntity
     {
-        public string SalesOrderNo { get; set; }
-        public int SalesStatusId { get; set; }
-        public long CustomerId { get; set; } 
+        public string OrderNo { get; set; }
         public DateTime SalesDate { get; set; }
-        public DateTime DeliveryDate { get; set; }
-        public string DeliveryAddress { get; set; }
-        public string TermsAndCondition { get; set; }
-        public string Description { get; set; }
-        public string PaymentType { get; set; }
-        public long? StorehouseId { get; set; }
+        public long StorehouseId { get; set; }
         public BusinessCenter Storehouse { get; set; }
+        public long CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        public decimal OverallDiscount { get; set; }
+        public string Description { get; set; }
+        public string TermsAndCondition { get; set; }
+        public DateTime? DeliveryDate { get; set; }
+        public string DeliveryAddress { get; set; }
+     
+        public int OrderStatusId { get; set; } //SalesOrderStatusEnum
+        public int PaymentStatusId { get; set; } //PaymentStatusEnum
+
     }
 }
--- app.EntityModel/AppModels/SalesOrderDetails.cs
+++ app.EntityModel/AppModels/SalesOrderDetails.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace app.EntityModel.AppModels
+﻿namespace app.EntityModel.AppModels
 {
     public class SalesOrderDetails : BaseEntity
     {
@@ -14,10 +8,7 @@ namespace app.EntityModel.AppModels
         public long UnitId { get; set; }
         public Unit Unit { get; set; }
         public decimal SalesQty { get; set; }
-        public decimal SalesRate { get; set; }
-        public decimal SalesAmount { get; set; }
-        public DateTime SalesDate { get; set; }
-        public DateTime DeliveryDate { get; set; }
+        public decimal SalesPrice { get; set; }
         public decimal Discount { get; set; }
         public decimal TotalAmount { get; set; }
         public string Remarks { get; set; }
