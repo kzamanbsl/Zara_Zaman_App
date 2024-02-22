1fd552da189f236623f9b130cd2ce210bab28855
Shimul-Aashraf <ashraf17cse@gmail.com>
2024-02-22 10:39:52 +0600

    ServiceCenter pagination updated

--- app.BusinessLogic/ServiceCenterServices/ServiceCenterService.cs
+++ app.BusinessLogic/ServiceCenterServices/ServiceCenterService.cs
@@ -1,8 +1,11 @@
 ﻿using app.EntityModel.AppModels;
+using app.EntityModel.DataTablePaginationModels;
 using app.Infrastructure;
 using app.Infrastructure.Auth;
 using app.Infrastructure.Repository;
+using app.Services.DepartmentServices;
 using app.Utility;
+using Microsoft.EntityFrameworkCore;
 
 namespace app.Services.ServiceCenterServices
 {
@@ -88,6 +91,47 @@ namespace app.Services.ServiceCenterServices
                                                                 BusinessCenterTypeId = t1.BusinessCenterTypeId,
                                                             }).AsEnumerable());
             return model;
+        }
+        public async Task<DataTablePagination<ServiceCenterSearchDto>> SearchAsync(DataTablePagination<ServiceCenterSearchDto> searchDto)
+        {
+            var searchResult = _dbContext.BusinessCenter.AsNoTracking();
+
+            var searchModel = searchDto.SearchVm;
+            var filter = searchDto?.Search?.Value?.Trim();
+
+            if (!string.IsNullOrEmpty(filter))
+            {
+                filter = filter.ToLower();
+                searchResult = searchResult.Where(c =>
+                    c.Name.ToLower().Contains(filter)
+                    || c.Code.ToLower().Contains(filter)
+                    || c.Location.ToLower().Contains(filter)
+                );
+            }
+
+            var pageSize = searchDto.Length ?? 0;
+            var skip = searchDto.Start ?? 0;
+
+            var totalRecords = await searchResult.CountAsync();
+            if (totalRecords <= 0) return searchDto;
+
+            searchDto.RecordsTotal = totalRecords;
+            searchDto.RecordsFiltered = totalRecords;
+            List<BusinessCenter> filteredDataList = await searchResult.OrderByDescending(c => c.Id).Skip(skip).Take(pageSize).ToListAsync();
+
+            var sl = searchDto.Start ?? 0;
+            searchDto.Data = filteredDataList.Select(c => new ServiceCenterSearchDto()
+            {
+                SerialNo = ++sl,
+                Id = c.Id,
+                Name = c.Name,
+                Code = c.Code,
+                Location = c.Location,
+                Description = c.Description,
+
+            }).ToList();
+
+            return searchDto;
         }
     }
 }
