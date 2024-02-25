ebe19adb0940af307c53e85aa9beb734876140b0
Shimul-Aashraf <ashraf17cse@gmail.com>
2024-02-25 11:11:14 +0600

    Shift&OfficeType Updated

--- app.WebApp/Controllers/ShiftController.cs
+++ app.WebApp/Controllers/ShiftController.cs
@@ -20,13 +20,6 @@ namespace app.WebApp.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Index()
-        {
-            var result = await _iService.GetAllRecord();
-            return View(result);
-        }
-
-        [HttpGet]
         public async Task<IActionResult> AddRecord()
         {
             ShiftViewModel viewModel = new ShiftViewModel();
