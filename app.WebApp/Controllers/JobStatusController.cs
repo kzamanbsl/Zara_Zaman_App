eea6520b7484cc63289198ba1906a2eea35af188
Shimul-Aashraf <ashraf17cse@gmail.com>
2024-02-25 11:38:06 +0600

    JobStatus&DropDownItem Updated

--- app.WebApp/Controllers/JobStatusController.cs
+++ app.WebApp/Controllers/JobStatusController.cs
@@ -15,13 +15,6 @@ namespace app.WebApp.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Index()
-        {
-            var result = await _iService.GetAllRecord();
-            return View(result);
-        }
-
-        [HttpGet]
         public async Task<IActionResult> AddRecord()
         {
             JobStatusViewModel viewModel = new JobStatusViewModel();
