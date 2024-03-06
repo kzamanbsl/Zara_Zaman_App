528b1004a5ebc3f53703abfebe602b40d4cb5951
Emran321 <rony.ehossain@gmail.com>
2024-03-06 18:30:18 +0600

    Multiple SerialAndModelNo complete

--- app.BusinessLogic/SalesProductDetailServices/SalesProductDetailViewModel.cs
+++ app.BusinessLogic/SalesProductDetailServices/SalesProductDetailViewModel.cs
@@ -6,12 +6,9 @@ namespace app.Services.SalesProductDetailServices
     {
         public long SalesOrderDetailsId { get; set; }
         public SalesOrderDetails SalesOrderDetails { get; set; }
-        public DateTime? WarrantyFormDate { get; set; }
-        public DateTime? WarrantyToDate { get; set; }
-       // public string SerialNo { get; set; }
-        //public string ModelNo { get; set; }
-        public bool IsForService { get; set; }
-
+        public DateTime?[] WarrantyFormDate { get; set; }
+        public DateTime?[] WarrantyToDate { get; set; }
+        public List<bool?>  IsForService { get; set; }
         public List<string> SerialNo { get; set; }
         public List<string> ModelNo { get; set; }
         public SalesProductDetailViewModel SalesProductDetailsVM { get; set; }
